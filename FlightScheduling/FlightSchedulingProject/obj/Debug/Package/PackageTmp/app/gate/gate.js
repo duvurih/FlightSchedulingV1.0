2a7af25931b175a17a9b650eb69fabc167982030
Hari Krishna Duvuri <hk_india@hotmail.com>
2017-06-19 10:03:43 +1000

    Hiding the table columns on mobile

---
+++ FlightScheduling/FlightSchedulingProject/obj/Debug/Package/PackageTmp/app/gate/gate.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'gate';
+    angular.module('app').controller(controllerId, ['common', gate]);
+
+    function gate(common) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Gate';
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated Gate View'); });
+        }
+    }
+})();
